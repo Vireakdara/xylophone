a37d06eb14ffa0128446d20d6a553e9eb2e00c25
Ly Vireakdara <lyvireakdara@gmail.com>
2021-01-22 08:44:16 +0700

    fix package

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -8,7 +9,19 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: Center(
+              child: FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+
+                  // call this method when desired
+                  player.play('note1.wav');
+                },
+                child: Text("Click Me"),
+              ),
+            ),
+          ),
         ),
       ),
     );
